b7169cabf3bb6b3b0a9425368f83964873ef5cf8
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-03-07 18:15:49 +0100

    creando proyecto vacio

---
+++ src/backend/fitConnection/src/test/java/com/daniel/app/backend/FitConnectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitConnectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
