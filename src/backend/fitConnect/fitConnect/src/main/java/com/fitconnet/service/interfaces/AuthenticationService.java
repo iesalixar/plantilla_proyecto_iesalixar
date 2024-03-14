9e2f02f8461069a5b6115f33ee4be04668f97613
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-03-14 12:13:47 +0100

    package

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/service/interfaces/AuthenticationService.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/service/interfaces/AuthenticationService.java
@@ -1,9 +1,5 @@
 package com.fitconnet.service.interfaces;
 
-import com.api.productos.dto.request.SignUpRequest;
-import com.api.productos.dto.request.SigninRequest;
-import com.api.productos.dto.response.user.JwtAuthenticationResponse;
-
 public interface AuthenticationService {
 
 	/** REGISTRO */
