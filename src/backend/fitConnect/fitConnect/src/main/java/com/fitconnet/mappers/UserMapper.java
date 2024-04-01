8da1fb30c917e0b67373fd77cd8f35bcd243027c
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-04-01 11:54:11 +0200

    [BACKEND] dejando solo algunos mapeos

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/mappers/UserMapper.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/mappers/UserMapper.java
@@ -1,30 +1,38 @@
 package com.fitconnet.mappers;
 
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import com.fitconnet.dto.UserDTO;
 import com.fitconnet.persitence.model.User;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface UserMapper {
 
-	@Mapping(source = "id", target = "id")
-	@Mapping(source = "firstName", target = "firstName")
-	@Mapping(source = "lastName", target = "lastName")
-	@Mapping(source = "email", target = "email")
-	UserDTO toUserDTO(User user);
+	public UserDTO toUserDTO(User user);
 
+	@InheritInverseConfiguration
 	User toUser(UserDTO userDTO);
 
-	List<UserDTO> toUserDTOList(Iterable<User> iterable);
 
 	void updateUser(User target, @MappingTarget User source);
 
-	Set<User> toUserList(List<UserDTO> participants);
+	Set<User> toUserList(Set<UserDTO> participants);
+	
+	 default Set<UserDTO> toUserDTOList(Iterable<User> users) {
+	        if (users == null) {
+	            return null;
+	        }
 
+	        Set<UserDTO> userDTOs = new HashSet<>();
+	        for (User user : users) {
+	            UserDTO userDTO = toUserDTO(user);
+	            userDTOs.add(userDTO);
+	        }
+	        return userDTOs;
+	    }
 }
