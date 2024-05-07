69f8c8f63fcbcbe1909c3a2b23b57f2c196ff115
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-07 13:21:34 +0200

    [BACKEND] allowing methods to CR

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/config/CorsConfig.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/**").allowedOrigins("http://localhost:3000")
-						.allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "UPDATE").allowedHeaders("*");
 			}
 		};
 	}
