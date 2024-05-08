5c88a650c52c24b5e284e63cc2b6cb0ea37a844f
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-08 11:48:47 +0200

    [BACKEND]
    [feature/services] Adding @Repository notations.

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/persitence/repository/NotificationRepository.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/persitence/repository/NotificationRepository.java
@@ -1,9 +1,11 @@
 package com.fitconnet.persitence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fitconnet.persitence.model.Notification;
 
+@Repository("notificationRepository")
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
 }
