3affeb9beabc7ad08f78a069513aa689786c2020
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-08 11:42:00 +0200

    [BACKEND]
    [feature/controller] created the notification rest controller.

---
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/controller/notification/NotificationController.java
@@ -0,0 +1,10 @@
+package com.fitconnet.controller.notification;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/notification")
+public class NotificationController {
+
+}
