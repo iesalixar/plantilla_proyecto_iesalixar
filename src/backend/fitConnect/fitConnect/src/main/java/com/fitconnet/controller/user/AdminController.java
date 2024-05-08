30ebce47c9879a4a042ffa41af28b5807e6335e5
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-08 13:53:03 +0200

    [BACKEND]
    [Branch: feature/controller]
    
    Signed-off-by: Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/controller/user/AdminController.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/controller/user/AdminController.java
@@ -1,5 +1,8 @@
 package com.fitconnet.controller.user;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fitconnet.persitence.model.Notification;
 import com.fitconnet.persitence.model.User;
 import com.fitconnet.service.interfaces.UserServiceI;
 
@@ -32,15 +36,37 @@ public class AdminController {
 
 	@GetMapping
 	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
-	public ResponseEntity<Set<User>> showUsers() {
+	public ResponseEntity<List<User>> showUsers() {
 		logger.info("## AuthorizationAdminController :: showUsers");
-		Set<User> userList = userService.getAllUsers();
+
+		// Obtener el conjunto de usuarios
+		Set<User> userSet = userService.getAllUsers();
+
+		// Filtrar los usuarios que tienen el rol 'USER'
+		userSet.removeIf(user -> !user.getRoles().contains("ROLE_USER"));
+
+		// Convertir el conjunto a una lista
+		List<User> userList = new ArrayList<>(userSet);
+
+		// Ordenar la lista por userName
+		userList.sort(Comparator.comparing(User::getUserName));
+
 		return ResponseEntity.ok(userList);
+	}
+
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public ResponseEntity<Set<Notification>> showNotifications() {
+		logger.info("## AuthorizationAdminController :: showNotifications");
+		// Set<Notification> notificationsSet = userService.getAllNotifications();
+		return ResponseEntity.ok(notificationsSet);
+
 	}
 
 	@PutMapping("/{id}")
 	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+		logger.info("## AuthorizationAdminController :: updateUser");
 		User updatedUser = userService.updateUser(id, user);
 		return ResponseEntity.ok(updatedUser);
 	}
@@ -48,6 +74,8 @@ public class AdminController {
 	@DeleteMapping("/{id}")
 	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+		logger.info("## AuthorizationAdminController :: deleteUser");
+
 		User deletedUser = userService.deleteById(id);
 		return ResponseEntity.ok(deletedUser);
 	}
