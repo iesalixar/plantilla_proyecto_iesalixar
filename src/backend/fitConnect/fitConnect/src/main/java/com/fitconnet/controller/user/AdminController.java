35a620d5c847dd9c2e5a35a0dd6fce2865a2f344
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-09 20:15:05 +0200

    [BACKEND]
    [Branch: feature/controller] Coding refactor due new service inyection
    to processing reponses and fill GET endpoint.
    
    Signed-off-by: Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/controller/user/AdminController.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/controller/user/AdminController.java
@@ -1,14 +1,16 @@
 package com.fitconnet.controller.user;
 
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,14 +26,17 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.fitconnet.dto.response.error.ErrorDetailsResponse;
 import com.fitconnet.error.GlobalExceptionHandler;
+import com.fitconnet.persitence.model.Activity;
 import com.fitconnet.persitence.model.Notification;
 import com.fitconnet.persitence.model.User;
 import com.fitconnet.service.interfaces.ActivityServiceI;
 import com.fitconnet.service.interfaces.NotificationServiceI;
+import com.fitconnet.service.interfaces.ProcessingResponseI;
 import com.fitconnet.service.interfaces.UserServiceI;
+import com.fitconnet.utils.Constants;
 
 @RestController
-@RequestMapping("/api/v1/admins")
+@RequestMapping("/api/v1/admin")
 public class AdminController {
 
 	@Qualifier("userService")
@@ -42,75 +47,88 @@ public class AdminController {
 	private final NotificationServiceI notificationService;
 	@Qualifier("globalExceptionHandler")
 	private final GlobalExceptionHandler globalExceptionHandler;
-	private final Logger LOG = LoggerFactory.getLogger(UserController.class);
+	@Qualifier("processingResponseI")
+	private final ProcessingResponseI processingResponseI;
+
+	private final Logger logger = LoggerFactory.getLogger(AdminController.class);
 
 	public AdminController(UserServiceI userService, ActivityServiceI activityService,
-			NotificationServiceI notificationService, GlobalExceptionHandler globalExceptionHandler) {
+			NotificationServiceI notificationService, GlobalExceptionHandler globalExceptionHandler,
+			ProcessingResponseI processingResponseI) {
 		this.userService = userService;
 		this.activityService = activityService;
 		this.notificationService = notificationService;
 		this.globalExceptionHandler = globalExceptionHandler;
+		this.processingResponseI = processingResponseI;
 	}
 
-//	@GetMapping("/dashboard")
-//	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
-//	public ResponseEntity<DashboardData> showDashboard() {
-//		LOG.info("## AdminController :: showDashboard");
-//
-//		DashboardData dashboardData = new DashboardData();
-//
-//		// Obtener todos los usuarios y filtrarlos
-//		Set<User> allUsers = userService.getAllUsers();
-//		allUsers.removeIf(user -> !user.getRoles().contains("ROLE_USER"));
-//		List<User> userList = new ArrayList<>(allUsers);
-//		userList.sort(Comparator.comparing(User::getUserName));
-//		dashboardData.setUsers(userList);
-//
-//		// Obtener todas las actividades
-//		Set<Activity> allActivities = activityService.getAll();
-//		dashboardData.setActivities(allActivities);
-//
-//		// Obtener todas las notificaciones
-//		List<Notification> allNotifications = notificationService.getAllNotifications();
-//		dashboardData.setNotifications(allNotifications);
-//
-//		return ResponseEntity.ok(dashboardData);
-//	}
+	@GetMapping("/dashboard")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public ResponseEntity<Map<String, Object>> showDashboard() {
+		logger.info("## AdminController :: showDashboard");
+
+		List<User> userList = userService.userSetToSortedList();
+
+		Optional<Set<Activity>> activities = activityService.getAllActivity();
+
+		Map<String, Object> dashboardData = new HashMap<>();
+		dashboardData.put("users", userList);
+		dashboardData.put("activities", activities.orElse(new HashSet<>()));
+
+		return ResponseEntity.ok().body(dashboardData);
+	}
 
 	@GetMapping("/users")
 	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	public ResponseEntity<List<User>> showUsers() {
-		LOG.info("## AdminController :: showUsers");
-		Set<User> userSet = userService.getAllUsers();
-		userSet.removeIf(user -> !user.getRoles().contains("ROLE_USER"));
-		List<User> userList = new ArrayList<>(userSet);
-		userList.sort(Comparator.comparing(User::getUserName));
-		return ResponseEntity.ok(userList);
+		logger.info("## AdminController :: showUsers");
+		List<User> userList = userService.userSetToSortedList();
+		return ResponseEntity.ok().body(userList);
+	}
+
+	@GetMapping("/activities")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public ResponseEntity<Optional<Set<Activity>>> showActivities() {
+		logger.info("## AdminController :: showActivities");
+		Optional<Set<Activity>> activities = activityService.getAllActivity();
+		return ResponseEntity.ok().body(activities);
 	}
 
 	@GetMapping("/notifications")
 	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	public ResponseEntity<Optional<Set<Notification>>> showNotifications() {
-		LOG.info("## AdminController :: showNotifications");
+		logger.info("## AdminController :: showNotifications");
 		Optional<Set<Notification>> notifications = notificationService.getAll();
-		return ResponseEntity.ok(notifications);
+		return ResponseEntity.ok().body(notifications);
 
 	}
 
 	@PutMapping("/{id}")
 	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
-	public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
-		LOG.info("## AdminController :: updateUser");
-		User updatedUser = userService.updateUser(id, user);
-		return ResponseEntity.ok(updatedUser);
+	public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User user) {
+		logger.info("## AdminController :: updateUser");
+		ResponseEntity<String> response = null;
+		Optional<User> existingUser = userService.getUserMethod(id);
+		response = processingResponseI.processResponse(existingUser,
+				() -> ResponseEntity.status(HttpStatus.CONFLICT).body(Constants.USER_NOT_EXIST), () -> {
+					userService.updateUser(id, user);
+					return ResponseEntity.ok().body("Usuario actualizado");
+				});
+		return response;
 	}
 
 	@DeleteMapping("/{id}")
 	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
-	public ResponseEntity<User> deleteUser(@PathVariable Long id) {
-		LOG.info("## AdminController :: deleteUser");
-		User deletedUser = userService.deleteById(id);
-		return ResponseEntity.ok(deletedUser);
+	public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+		logger.info("## AdminController :: deleteUser");
+		ResponseEntity<String> response = null;
+		Optional<User> existingUser = userService.getUserMethod(id);
+		response = processingResponseI.processResponse(existingUser,
+				() -> ResponseEntity.status(HttpStatus.CONFLICT).body(Constants.USER_NOT_EXIST), () -> {
+					userService.deleteById(id);
+					return ResponseEntity.ok().body("Usuario ha sido eliminado exitosamente");
+				});
+		return response;
 	}
 
 	@ExceptionHandler(NoHandlerFoundException.class)
