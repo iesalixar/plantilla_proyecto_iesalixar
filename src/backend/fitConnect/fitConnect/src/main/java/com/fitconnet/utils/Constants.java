fab25f489fa403c6d4ef0b9ccd5f1c8f0ce89b70
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-09 20:16:02 +0200

    [BACKEND]
    [Branch: feature/controller] Added new utils as a constants class.
    
    Signed-off-by: Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>

---
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.fitconnet.utils;
+
+public class Constants {
+	public static final String USER_NOT_EXIST = "El usuario no existe";
+	public static final String USER_NOT_FOUND = "El usuario no encontrado";
+
+}
