1b406d448310c4101228b3176012e16ef182f61d
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-10 10:25:28 +0200

    [BACKEND]
    [Branch: develop] Moved AuthenticationController to auth package.
    
    Signed-off-by: Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/controller/auth/AuthenticationController.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/controller/auth/AuthenticationController.java
@@ -1,4 +1,4 @@
-package com.fitconnet.controller.user;
+package com.fitconnet.controller.auth;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
