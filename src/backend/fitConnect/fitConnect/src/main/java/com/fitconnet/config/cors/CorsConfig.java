ff3d20e04516ad78b6c4605e089faed7ad51fee5
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-10 17:11:44 +0200

    [BACKEND]
    [Branch: develop] updated cors settings.

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/config/cors/CorsConfig.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/config/cors/CorsConfig.java
@@ -1,21 +1,27 @@
 package com.fitconnet.config.cors;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:3000")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "UPDATE").allowedHeaders("*");
-			}
-		};
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "UPDATE", "PATCH"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+
+		return source;
 	}
 }
