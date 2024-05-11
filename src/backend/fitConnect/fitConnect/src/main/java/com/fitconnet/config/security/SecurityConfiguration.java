a2f48f4eb70fea147eed6fb60bb042eacbac7d87
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-11 19:33:13 +0200

    [BACKEND]
    [Branch: develop] Added rest of the endpoints to the security
    configuration.
    
    Signed-off-by: Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/config/security/SecurityConfiguration.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/config/security/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -24,12 +23,6 @@ import com.fitconnet.service.interfaces.entity.UserServiceI;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-	private static final String ACTIVITY_API_PATH = "/api/v1/activity";
-
-	private static final String ADMIN_API_PATH = "/api/v1/admin";
-
-	private static final String USER_API_PATH = "/api/v1/user";
-
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 	private final UserServiceI userService;
@@ -41,23 +34,26 @@ public class SecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		// TODO implementar los filtros de seguridad segun endpoint
-		http.csrf(AbstractHttpConfigurer::disable)
-				.authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**").permitAll()
-						.requestMatchers(HttpMethod.GET, "/api/v1/actividades/**")
-						.hasAnyAuthority(Role.ROLE_USER.toString(), Role.ROLE_ADMIN.toString())
-						.requestMatchers(HttpMethod.POST, "/api/v1/actividades/**")
-						.hasAnyAuthority(Role.ROLE_USER.toString(), Role.ROLE_ADMIN.toString()) // Permitir tanto a
-																								// usuarios como a
-																								// administradores crear
-																								// actividades
-						.requestMatchers(HttpMethod.PUT, ACTIVITY_API_PATH + "**")
-						.hasAuthority(Role.ROLE_ADMIN.toString())
-						.requestMatchers(HttpMethod.DELETE, ACTIVITY_API_PATH + "**")
-						.hasAuthority(Role.ROLE_ADMIN.toString())
-						.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-						.requestMatchers("/api/v1/admin/**").hasAuthority("ROLE_ADMIN").anyRequest().authenticated())
-				.sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+		http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(request -> request
+				// Permitir acceso a todos los métodos HTTP en /api/v1/auth para todos los roles
+				.requestMatchers("/api/v1/auth/**").permitAll()
+				// Permitir acceso de admin y usuario a todos los métodos HTTP en
+				// /api/v1/actividades
+				.requestMatchers("/api/v1/actividades/**")
+				.hasAnyAuthority(Role.ROLE_USER.toString(), Role.ROLE_ADMIN.toString())
+				// Permitir acceso de admin y usuario a todos los métodos HTTP en
+				// /api/v1/notification
+				.requestMatchers("/api/v1/notification/**")
+				.hasAnyAuthority(Role.ROLE_USER.toString(), Role.ROLE_ADMIN.toString())
+				// Permitir acceso de admin y usuario a todos los métodos HTTP en /api/v1/user
+				.requestMatchers("/api/v1/user/**")
+				.hasAnyAuthority(Role.ROLE_USER.toString(), Role.ROLE_ADMIN.toString())
+				// Restringir acceso a todos los métodos HTTP en /api/v1/admin para el rol de
+				// admin
+				.requestMatchers("/api/v1/admin/**").hasAnyAuthority(Role.ROLE_ADMIN.toString())
+				// Permitir acceso a la documentación de Swagger para todos los roles
+				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll().anyRequest()
+				.authenticated()).sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
 				.authenticationProvider(authenticationProvider())
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/utils/Constants.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/utils/Constants.java
@@ -5,4 +5,10 @@ public class Constants {
 	public static final String NOTIFICATION_NOT_FOUND = "Notificación no encontrada";
 	public static final String ACTIVITY_NOT_FOUND = "Actividad no encontrada";
 
+	public static final String ACTIVITY_API_PATH = "/api/v1/activity";
+
+	public static final String ADMIN_API_PATH = "/api/v1/admin";
+
+	public static final String USER_API_PATH = "/api/v1/user";
+
 }
