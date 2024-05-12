4a135567e6ee3e39b4e50c41dc97f693ebc9028f
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-05-12 09:57:11 +0200

    Add javadoc CorsConfig.java

--- src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/config/cors/CorsConfig.java
+++ src/backend/fitConnect/fitConnect/src/main/java/com/fitconnet/config/cors/CorsConfig.java
@@ -8,17 +8,34 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+/**
+ * Configuration class for Cross-Origin Resource Sharing (CORS) setup. Enables
+ * CORS for all origins, with specified allowed methods and headers.
+ */
 @Configuration
 public class CorsConfig {
 
+	/**
+	 * Defines a CorsConfigurationSource bean to configure CORS for the application.
+	 *
+	 * @return CorsConfigurationSource object configured with allowed origins,
+	 *         methods, and headers.
+	 */
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 
+		// Allow requests from all origins
 		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+
+		// Allow specified HTTP methods
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "UPDATE", "PATCH"));
+
+		// Allow all headers
 		corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
 
+		// Create a UrlBasedCorsConfigurationSource and register the CORS configuration
+		// for all endpoints
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", corsConfiguration);
 
