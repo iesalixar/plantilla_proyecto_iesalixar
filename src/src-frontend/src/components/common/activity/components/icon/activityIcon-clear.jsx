0fe07d47d7501a56414852ad6893d1398747506b
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-06-09 13:55:49 +0200

    Added dots icon to activityIcon-clear and dark

--- src/src-frontend/src/components/common/activity/components/icon/activityIcon-clear.jsx
+++ src/src-frontend/src/components/common/activity/components/icon/activityIcon-clear.jsx
@@ -45,5 +45,20 @@ const CommentIconClear = (props) => (
     </svg>
 );
 
+const DotsIconClear = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1.8em"
+        height="1.8em"
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path
+            fill="#00484C"
+            d="M12 16a2 2 0 0 1 2 2 2 2 0 0 1-2 2 2 2 0 0 1-2-2 2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2 2 2 0 0 1-2 2 2 2 0 0 1-2-2 2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2 2 2 0 0 1-2 2 2 2 0 0 1-2-2 2 2 0 0 1 2-2"
+        />
+    </svg>
+);
 
-export { DefaultHearIcontClear, FullHeartIconClear, CommentIconClear };
+
+export { DefaultHearIcontClear, FullHeartIconClear, CommentIconClear, DotsIconClear };
--- src/src-frontend/src/components/common/activity/components/icon/activityIcon-dark.jsx
+++ src/src-frontend/src/components/common/activity/components/icon/activityIcon-dark.jsx
@@ -44,6 +44,20 @@ const CommentIconDark = (props) => (
         />
     </svg>
 );
+const DotsIconDark = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1.8em"
+        height="1.8em"
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path
+            fill="#84E7ED"
+            d="M12 16a2 2 0 0 1 2 2 2 2 0 0 1-2 2 2 2 0 0 1-2-2 2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2 2 2 0 0 1-2 2 2 2 0 0 1-2-2 2 2 0 0 1 2-2m0-6a2 2 0 0 1 2 2 2 2 0 0 1-2 2 2 2 0 0 1-2-2 2 2 0 0 1 2-2"
+        />
+    </svg>
+);
 
 
-export { DefaultHearIcontDark, FullHeartIconDark, CommentIconDark };
+export { DefaultHearIcontDark, FullHeartIconDark, CommentIconDark, DotsIconDark };
