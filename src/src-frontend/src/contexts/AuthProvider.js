8f1468b670d42d988ebb0ea181174f01c5aac960
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-06-10 20:41:35 +0200

    Update AuthProvider.js
    Deleted un used useInterval and API calls

--- src/src-frontend/src/contexts/AuthProvider.js
+++ src/src-frontend/src/contexts/AuthProvider.js
@@ -1,26 +1,15 @@
 import React, { createContext, useContext, useState } from 'react';
 import useAuth from '../hooks/useAuth';
-import useInterval from '../hooks/useInterval';
-import { fetchAllUsers, fetchUserData } from '../service/userService';
+
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const auth = useAuth();
-  const { userData, updateUser } = auth;
-  const token = userData ? userData.token : null;
-  const [allUsers, setAllUsers] = useState([]);
-
-  useInterval(async () => {
-    if (token && userData) {
-      await fetchUserData(token, userData.user.id, updateUser);
-      const users = await fetchAllUsers(token);
-      setAllUsers(users);
-    }
-  }, 60000);
+  const { userData } = auth;
 
   return (
-    <AuthContext.Provider value={{ ...auth, allUsers }}>
+    <AuthContext.Provider value={{ ...auth }}>
       {children}
     </AuthContext.Provider>
   );
