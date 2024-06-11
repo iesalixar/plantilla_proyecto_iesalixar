66350a6a8a3b180d3706c990b2028347467f6e6c
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-06-11 13:36:06 +0200

    Update App.js
    Improve routing:
    - Added logic to handle user roles path accebilty

--- src/src-frontend/src/App.js
+++ src/src-frontend/src/App.js
@@ -1,32 +1,65 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthProvider.js';
 import { ThemeProvider } from './contexts/ThemeProvider.js';
 import { ScreenProvider } from './contexts/ScreenProvider.js';
 import { ModalProvider } from './contexts/ModalProvider.js';
 
-import ProtectedRoute from './service/routerProtectionService.js';
+import ProtectedRoute from './service/ProtectedRouterService.js';
 
 import HomePage from './pages/home/homePage';
-import AuthPage from './pages/auth/authPage.jsx';
 import DashboardPage from './pages/admin/dashboardPage.jsx';
 import UserTable from './components/common/table/userTable.jsx';
+import AuthPage from './pages/auth/authPage.jsx';
 
 import "@fontsource/roboto";
 import './app.scss'
 
 function App() {
   document.title = 'FitConnet';
+  const isLoggedIn = window.sessionStorage.getItem("loggedIn");
+  const userType = window.sessionStorage.getItem("userType");
+  const isAdmin = () => {
+    if (userType && userType == ('ROLE_ADMIN')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
-        <Route path="/login" element={<AuthPage />} />
-        <Route path="/register" element={<AuthPage />} />
-        <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
-        <Route path="/pruebas" element={<UserTable />} />
+        {/* unauthorized route */}
+        {!isLoggedIn && (
+          <>
+            <Route path="/login" element={<AuthPage />} />
+            <Route path="/register" element={<AuthPage />} />
+            <Route path="/" element={<Navigate to="/login" />} />
+          </>
+        )}
+        {/* ProtectedRoutes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/login" element={<Navigate to="/" />} />
+          <Route path="/register" element={<Navigate to="/" />} />
+          {isAdmin() ? (
+            <>
+              <Route path="/" element={<Navigate to="/dashboard" />} />
+              <Route path="/dashboard" element={< DashboardPage />} />
+              <Route path="/pruebas" element={< UserTable />} />
+
+
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/home" element={<Navigate to="/" />} />
+            </>
+          )}
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
